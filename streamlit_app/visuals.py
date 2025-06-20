9482fa3c69d518df9d78bfe06a1220238ac28f6b
saurav singh <sauravsingh20241080@gmail.com>
2025-06-20 14:25:54 +0530

    Update visuals.py

--- streamlit_app/visuals.py
+++ streamlit_app/visuals.py
@@ -1,4 +1,3 @@
-# streamlit_app/visuals.py
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
@@ -7,17 +6,14 @@ def show_graphs(df):
     col1, col2 = st.columns(2)
 
     with col1:
-        st.write("### Area vs Price")
         fig1, ax1 = plt.subplots()
-        sns.scatterplot(x="area", y="price", data=df, ax=ax1)
-        ax1.set_xlabel("Area (sq. ft)")
-        ax1.set_ylabel("Price (Lakhs)")
+        sns.scatterplot(data=df, x="area", y="price", hue="location", ax=ax1)
+        plt.title("Area vs Price")
         st.pyplot(fig1)
 
     with col2:
-        st.write("### BHK vs Price")
         fig2, ax2 = plt.subplots()
-        sns.boxplot(x="bhk", y="price", data=df, ax=ax2)
-        ax2.set_xlabel("BHK")
-        ax2.set_ylabel("Price (Lakhs)")
+        sns.boxplot(data=df, x="bhk", y="price", ax=ax2)
+        plt.title("BHK vs Price")
         st.pyplot(fig2)
+
